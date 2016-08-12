0f1f83f831b816fab50a7a298ae4a0604a39ce2a
Jordan <jordan@curalate.com>
2016-08-12 16:55:22 -0700

    updates

--- js/custom-reveal.js
+++ js/custom-reveal.js
@@ -3,8 +3,8 @@ var travelReveal = window.travelReveal = {};
 travelReveal.data = [
   {
     "class": 'first-tag',
-    "iconUrl": "",
-    "thumbnailUrl": "",
+    "iconUrl": "//s3.amazonaws.com/curalate-public-assets/curalate-widget-customizations/custom-homepage-reveal/images/Blog.svg",
+    "thumbnailUrl": "//s3.amazonaws.com/curalate-public-assets/curalate-widget-customizations/custom-homepage-reveal/images/coffee.jpg",
     "title": "Horse Ranch Mountain",
     "subtext": "Summit - 8,726ft",
     "cta": "Learn More",
@@ -12,35 +12,35 @@ travelReveal.data = [
   },
   {
     "class": 'second-tag',
-    "iconUrl": "",
-    "thumbnailUrl": "",
+    "iconUrl": "//s3.amazonaws.com/curalate-public-assets/curalate-widget-customizations/custom-homepage-reveal/images/Cloud.svg",
+    "thumbnailUrl": "//s3.amazonaws.com/curalate-public-assets/curalate-widget-customizations/custom-homepage-reveal/images/weather.jpg",
     "title": "Springdate, Utah",
     "subtext": "Partly Sunny, 63°",
     "cta": "See Forecast",
-    "link": "",
+    "link": ""
   },
   {
     "class": 'third-tag',
-    "iconUrl": "",
-    "thumbnailUrl": "",
+    "iconUrl": "//s3.amazonaws.com/curalate-public-assets/curalate-widget-customizations/custom-homepage-reveal/images/Ticket.svg",
+    "thumbnailUrl": "//s3.amazonaws.com/curalate-public-assets/curalate-widget-customizations/custom-homepage-reveal/images/dest.jpg",
     "title": "Zion National Park",
     "subtext": "nps.gov/zion",
     "cta": "Get Tickets",
-    "link": "",
+    "link": ""
   },
   {
     "class": 'fourth-tag',
-    "iconUrl": "",
-    "thumbnailUrl": "",
+    "iconUrl": "//s3.amazonaws.com/curalate-public-assets/curalate-widget-customizations/custom-homepage-reveal/images/Tag.svg",
+    "thumbnailUrl": "//s3.amazonaws.com/curalate-public-assets/curalate-widget-customizations/custom-homepage-reveal/images/hat.jpg",
     "title": "SF Baseball Cap",
     "subtext": "$49 | Habiliment",
     "cta": "Shop",
-    "link": "",
+    "link": ""
   },
   {
     "class": 'fifth-tag',
-    "iconUrl": "",
-    "thumbnailUrl": "",
+    "iconUrl": "//s3.amazonaws.com/curalate-public-assets/curalate-widget-customizations/custom-homepage-reveal/images/Tag.svg",
+    "thumbnailUrl": "//s3.amazonaws.com/curalate-public-assets/curalate-widget-customizations/custom-homepage-reveal/images/backpack.jpg",
     "title": "Campus Backpack",
     "subtext": "$99 | Habiliment",
     "cta": "Shop",
@@ -71,9 +71,8 @@ travelReveal.createMarker = function(place) {
   });
 };
 
-travelReveal.formWeatherCall = function(endpoint, data, apiKey) {
-  console.log(data);
-  return endpoint + '?lat=' + data.lat + '&lon=' + data.lng + '&appid=' + apiKey + '&units=imperial';
+travelReveal.formWeatherCall = function(endpoint, data, yql) {
+  return endpoint + yql + '"(' + data.lat + ',' + data.lng + ')")&format=json';
 };
 
 travelReveal.formLocCall = function(endpoint, data, apiKey) {
@@ -82,19 +81,17 @@ travelReveal.formLocCall = function(endpoint, data, apiKey) {
 
 travelReveal.apiCalls = function() {
   var that = this;
-  var GEOIP_ENDPOINT = 'http://freegeoip.net/json/';
-  var WEATHER_ENDPOINT = 'http://api.openweathermap.org/data/2.5/weather';
+  var GEOIP_ENDPOINT = '//freegeoip.net/json/';
+  var WEATHER_ENDPOINT = '//query.yahooapis.com/v1/public/yql?q=';
+  var WEATHER_YQL = 'select * from weather.forecast where woeid in (SELECT woeid FROM geo.places WHERE text=';
   var GMAPS_API_KEY = 'AIzaSyCDuEW4yfbf4YrP3mrAGxJcvSuCHzQRiUE';
   var GMAPS_LOC_ENDPOINT = 'https://maps.googleapis.com/maps/api/geocode/json?';
   var GMAPS_LOCATION_KEY = 'AIzaSyDFooO1X9-FoPHqXuKiMgB6kae3YNHqJto';
-  var WEATHER_API_KEY = 'aa67d47c2cc3840547b7ebe3e0b2914c';
-  var lat,
-      lon;
 
   var request = $.ajax(GEOIP_ENDPOINT, { dataType: "jsonp" }),
       gmapsRequest = request.then(function(response) {
-            lat = response.latitude;
-            lon = response.longitude;
+            that.lat = response.latitude;
+            that.lon = response.longitude;
             return $.getScript('https://maps.googleapis.com/maps/api/js?key=' + GMAPS_API_KEY + '&libraries=places');
           },
           locRequest = request.then(function(response) {
@@ -104,15 +101,21 @@ travelReveal.apiCalls = function() {
 
   gmapsRequest.done(function() {
     var $tag = $('.first-tag .tag-content');
+
     $tag.append('<div id="map"></div>');
-    var loc = new google.maps.LatLng(lat, lon);
+    var loc = new google.maps.LatLng(that.lat, that.lon);
     that.infowindow = new google.maps.InfoWindow();
 
-    console.log($tag);
+
+    $tag.find('.tag-title').text('Coffee Shops');
+
+    var zip = '';
+
+    $tag.find('.tag-subtext').text('Locations Near:' + zip);
 
     that.map = new google.maps.Map($('#map')[0], {
       center: loc,
-      zoom: 15
+      zoom: 12
     });
 
     var request = {
@@ -135,17 +138,15 @@ travelReveal.apiCalls = function() {
   });
 
   var weatherRequest = locRequest.then(function(response) {
-    console.log(response);
     that.location = response.results[0];
-    return $.ajax(travelReveal.formWeatherCall(WEATHER_ENDPOINT, that.location.geometry.location, WEATHER_API_KEY));
+    return $.ajax(travelReveal.formWeatherCall(WEATHER_ENDPOINT, that.location.geometry.location, WEATHER_YQL));
   });
 
   weatherRequest.done(function( data ) {
-    console.log(that.location);
-    var temp = data.main.temp;
-    var description = data.weather[0].description;
+    var dataObj = data.query.results.channel;
+    var temp = dataObj.item.condition.temp;
+    var description = dataObj.item.condition.text;
     var address = that.location.address_components[1].short_name + ' ' + that.location.address_components[4].long_name;
-    console.log(address);
     var $container = $('.second-tag');
     $container.find('.tag-title').text(address);
     $container.find('.tag-subtext').text(description + ', ' + temp);
@@ -172,15 +173,17 @@ travelReveal.appendHtml = function(tag) {
       '</div>';
 
   this.$widget.append(html);
+
   this.$widget.on('mouseover', '.tag-icon-wrapper', function() {
     $(this).addClass('active');
-  });
-  this.$widget.on('mouseout', '.tag-icon-wrapper', function() {
-    $(this).removeClass('active');
-  });
-  this.$widget.on('click', '.tag-icon-wrapper', function() {
     $(this).siblings('.tag-content').addClass('active');
   });
+
+  this.$widget.on('mouseleave', '.tag', function() {
+    $(this).find('.tag-icon-wrapper').removeClass('active');
+    $(this).find('.tag-content').removeClass('active');
+  });
+
 };
 
 
